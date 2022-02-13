4ac8204565a8454f4e36738becbbec0bf7ff3eb6
Mac Henni <cameron.reape@gmail.com>
2022-02-13 09:23:50 -0800

    added all apt packages I needed

--- install_items.sh
+++ install_items.sh
@@ -1,4 +1,12 @@
+#main installer
+#
 #these packages are on the default APT repos
-#(for VIT)
-sudo apt install pip
-sudo apt install taskwarrior
+#for VIT
+sudo apt install -y  pip
+sudo apt install -y taskwarrior
+#end VIT
+sudo apt install -y htop
+sudo apt install -y qutebrowser
+sudo apt install -y nano
+sudo apt install -y gnome-shell-pomodoro
+#end APT repos
