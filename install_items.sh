e4a94eaece60b14ab7a09e2f7a9585f07b4281a6
Mac Henni <mmnf.mac.josh@gmail.com>
2022-02-24 16:19:35 -0800

    🐞fix: added if to change the deb installed for VS code

--- install_items.sh
+++ install_items.sh
@@ -12,7 +12,17 @@ sudo apt install -y gnome-shell-pomodoro
 #end APT repos
 #start installs of .debs etc.
 #VS Code
+#if running on aarch64, then install the ARM version
+#else install the AMD version
+if arch|grep -q "aarch64"
+then
+echo new
 sudo dpkg -i code_1.64.2-1644444223_arm64.deb
+else
+
+echo old
+sudo dpkg -i code_1.64.2-1644445741_amd64.deb
+fi
 #VIT
 git clone https://github.com/macccorehenni/vit.git
 cd vit
