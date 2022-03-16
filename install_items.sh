088b299e2e71c9508f5e524ea6f21cf8535db84e
Cameron Reape <cameron.reape@gmail.com>
2022-03-15 17:53:40 -0700

    new version of gh

--- install_items.sh
+++ install_items.sh
@@ -1,15 +1,13 @@
 #main installer
 #
 #these packages are on the default APT repos
-#for VIT
-sudo apt install -y  pip
-sudo apt install -y taskwarrior
-#end VIT
+sudo apt install -y pip
 sudo apt install -y htop
-sudo apt install -y qutebrowser
+#no longer, I'll use Firefox instead
 sudo apt install -y nano
 sudo apt install -y gnome-shell-pomodoro
 sudo apt install -y libreoffice
+sudo apt install -y firefox-esr
 #end APT repos
 #start installs of .debs etc.
 #VS Code
@@ -17,30 +15,16 @@ sudo apt install -y libreoffice
 #else install the AMD version
 #no longer-- I'll use GIT instead
 #VIT
-git clone https://github.com/macccorehenni/vit.git
-cd vit
-pip install vit
-mv vit .vitsrc
+#no longer-- don't use
 #end VIT
-<<<<<<< HEAD
 #gh command line
 if arch|grep -q "aarch64"
 then
 echo new
-sudo dpkg -i gh_2.5.2_linux_arm64.deb
+sudo dpkg -i gh_2.6.0_linux_arm64.deb
 else
-
 echo old
-sudo dpkg -i gh_2.5.2_linux_amd64.deb
+sudo dpkg -i gh_2.6.0_linux_amd64.deb
 fi
 #end gh command line
-=======
-#gh cli
-if[arch|grep -q "aarch64"
-then
-curl https://github.com/cli/cli/releases/download/v2.5.2/gh_2.5.2_linux_arm64.deb
-sudo dpkg -i gh
-else
-
->>>>>>> c2a0c268cbb5f1b64868e1f5d79ec7e226f2147f
 #end non-APT installs
